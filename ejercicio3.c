96073257c30c4220afdcfb76d8c4a675997e009e
Nicolas, hellodah <87020804+nicolasorregoa@users.noreply.github.com>
2021-10-26 12:06:07 -0300

    Create ejercicio3.c

---
+++ ejercicio3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+ int i=0,n1[10]={0};
+  printf("ingrese dos numeros: ");
+   for(i=0;i<2;i++){
+    scanf("%d",&n1[i]);
+}
+   for(i=0;i<n1[1];i++){
+      printf("%d\n",n1[i]);
+}
+
+return 0;
+  }
+
+
+
+
+
+
+
