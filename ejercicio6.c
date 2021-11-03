1c0ac64a7694718a59a474604620f735f31b304a
Nicolas, hellodah <87020804+nicolasorregoa@users.noreply.github.com>
2021-11-03 14:42:47 +0000

    Update ejercicio6.c

--- ejercicio6.c
+++ ejercicio6.c
@@ -4,16 +4,13 @@ int main(){
  for(i=0;i<20;i++){
   printf("ingrese 20  sueldos: ");
    scanf("%d",&sueldo[i]);
-
   if(sueldo[i]>2000){
     mayor++;
  }
   else{
    menor++;
  }
-
 }
-
  printf("los que ganan mas de 2000 son: %d\n",mayor);
  printf("los que ganan menos de 2000 son: %d\n",menor);
   return 0;
